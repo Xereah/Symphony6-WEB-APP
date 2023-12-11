46ec570dda6faf7836b8bc9f319fc2ac47d571aa
Xereah <patrykstruzik@onet.pl>
2023-12-11 16:22:34 +0100

    first commit

---
+++ src/Command/FethPostCommand.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\HttpClient\HttpClient;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\User;
+use App\Entity\Post;
+
+#[AsCommand(
+    name: 'app:fetch-posts',
+    description: 'Fetch posts from API and save them to the database',
+)]
+class FethPostCommand extends Command
+{
+
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        parent::__construct();
+        $this->entityManager = $entityManager;
+    }
+
+    protected function configure(): void
+    {
+        $this->setName('app:fetch-posts')
+            ->setDescription('Fetch posts from API and save them to the database');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $httpClient = HttpClient::create();
+
+        $response = $httpClient->request('GET', 'https://jsonplaceholder.typicode.com/posts');
+        $postsData = $response->toArray();
+
+        $userResponse = $httpClient->request('GET', 'https://jsonplaceholder.typicode.com/users/');
+        $usersData = $userResponse->toArray();
+
+        foreach ($postsData as $postData) {          
+
+            $post = new Post();
+            $post->setTitle($postData['title']);
+            $post->setUserId($postData['userId']);
+            $post->setId($postData['id']);
+            $post->setBody($postData['body']);          
+        
+
+            $this->entityManager->persist($post);
+        }
+
+        foreach ($usersData as $userData) {
+           
+             $user = new User();
+             $user->setName($userData['name']);
+             $user->setUsername($userData['username']);
+             $user->setEmail($userData['email']);       
+        
+            $this->entityManager->persist($user);
+           
+        }
+
+        $this->entityManager->flush();
+
+        $output->writeln('Posts successfully fetched and saved to the database.');
+
+        return Command::SUCCESS;
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $username = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $email = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(?string $username): static
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
